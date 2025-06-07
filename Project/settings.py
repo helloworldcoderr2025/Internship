f75c3dfd671217fbdf8555cc994fea339759e50a
helloworldcoderr2025 <helloworldcoder2025@gmail.com>
2025-06-07 20:11:24 +0530

    Flatten Project folder structure by moving inner Project contents up

--- Project/settings.py
+++ Project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from supabase import create_client
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,6 +86,9 @@ DATABASES = {
 }
 
 
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -127,16 +130,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# Example SMTP settings for Gmail (adjust to your provider)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'temporaryu223@gmail.com'
-EMAIL_HOST_PASSWORD = 'sroq afba pxll ilxv'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-SUPABASE_URL = 'https://bzdozjdbyovuahcgvyhb.supabase.co'
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6ZG96amRieW92dWFoY2d2eWhiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzI0MjUwMSwiZXhwIjoyMDYyODE4NTAxfQ.ujblBvG206S4Xqure2ClCPZPJomHv9OUOCpEtYp-70E"
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
